d332d9e262a12af1c33d50b5a980179ed816f115
Guilherme da Luz <guilherme.daluz@outlook.com>
2016-09-21 11:12:42 -0300

    Adição da entidade Cidade

---
+++ JSF/src/main/java/br/com/model/entities/Cidade.java
@@ -0,0 +1,80 @@
+package br.com.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+/**
+ *
+ * @author guilherme
+ */
+@Entity
+@Table(name="cidade")
+public class Cidade implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name="IdCidade", nullable=false)
+    private Integer idCidade;
+    @Column(name="Nome", length=80,nullable=false)
+    private String nome;
+
+    @OneToMany
+    @ForeignKey(name="CidadeEndereço")
+    private List<Endereco> endereco;
+    
+    public Cidade() {
+    }
+
+    public Integer getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(Integer idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 13 * hash + Objects.hashCode(this.idCidade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cidade other = (Cidade) obj;
+        if (!Objects.equals(this.idCidade, other.idCidade)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
