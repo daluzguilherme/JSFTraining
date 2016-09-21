c559481b93070db4f44d120596ea21cc58d2c280
Guilherme da Luz <guilherme.daluz@outlook.com>
2016-09-21 12:00:12 -0300

    Adição de relacionamentos

--- JSF/src/main/java/br/com/model/entities/Endereco.java
+++ JSF/src/main/java/br/com/model/entities/Endereco.java
@@ -1,9 +1,153 @@
 package br.com.model.entities;
 
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
 /**
  *
  * @author guilherme
  */
-class Endereco {
+@Entity
+@Table(name = "endereco")
+public class Endereco implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name="IdEndereco", nullable = false)
+    private Integer idEndereco;
+    @Column(name="Bairro", length=80)
+    private String bairo;
+    @Column(name="NomeLogradouro", length=80)
+    private String bairro;
+    @Column(name="CEP", length=9)
+    private String cep;
+    @Column(name="Numero", length=9)
+    private Integer numero;
+    @Column(name="Complemento")
+    private Integer complemento;
+    
+    @OneToOne(optional=true, fetch=FetchType.LAZY)
+    @ForeignKey(name="EnderecoPessoa")
+    @JoinColumn(name="IdPessoa", referencedColumnName = "IdPessoa")
+    private Pessoa pessoa;
+    
+    @ManyToOne(optional=false, fetch = FetchType.LAZY)
+    @ForeignKey(name="EnderecoTipoLogradouro")
+    @JoinColumn(name="IdTipoLogradouro", referencedColumnName = "IdTipoLogradouro")
+    private TipoLogradouro tipoLogradouro;
+    
+    @ManyToOne(optional=false, fetch = FetchType.LAZY)
+    @ForeignKey(name="EnderecoEstado")
+    @JoinColumn(name="IdEstado", referencedColumnName = "IdEstado")
+    private Estado estado;
+    
+    //falta relacionamentos 16:26
+    
+
+    public Endereco() {
+    }
+
+    public Integer getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(Integer idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getBairo() {
+        return bairo;
+    }
+
+    public void setBairo(String bairo) {
+        this.bairo = bairo;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(Integer complemento) {
+        this.complemento = complemento;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    public TipoLogradouro getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(TipoLogradouro tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.idEndereco);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Endereco other = (Endereco) obj;
+        if (!Objects.equals(this.idEndereco, other.idEndereco)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
