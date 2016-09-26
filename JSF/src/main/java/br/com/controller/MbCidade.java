f8abb35728afa5ddd0a308d5a6abaf2bd4b309a5
Guilherme da Luz <guilherme.daluz@outlook.com>
2016-09-26 11:41:17 -0300

    Correção das entidades e MBeans

---
+++ JSF/src/main/java/br/com/controller/MbCidade.java
@@ -0,0 +1,86 @@
+package br.com.controller;
+
+import br.com.model.dao.HibernateDAO;
+import br.com.model.dao.InterfaceDAO;
+import br.com.model.entities.Cidade;
+import br.com.util.FacesContextUtil;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author guilherme
+ */
+@ManagedBean(name="mbCidade")
+@SessionScoped
+public class MbCidade implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Cidade cidade = new Cidade();
+    private List<Cidade> cidades;
+
+    public MbCidade() {
+    }
+    
+    private InterfaceDAO<Cidade> cidadeDAO() {
+        InterfaceDAO<Cidade> cidadeDAO = new HibernateDAO<Cidade>(Cidade.class, FacesContextUtil.getRequestSession());
+        return cidadeDAO;
+    }
+    
+    public String editCidade() {
+        return "/restrict/cadastrarCidade.faces";
+    }
+    
+    public String addCidade() {
+        if (cidade.getIdCidade() == null || cidade.getIdCidade() == 0) {
+            insertCidade();
+        } else {
+            updateCidade();
+        }
+        limpCidade();
+        return null;
+    }
+
+    private void insertCidade() {
+        cidadeDAO().save(cidade);
+        FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO, "Gravação efetuada com sucesso", ""));
+    }
+
+    private void updateCidade() {
+        cidadeDAO().update(cidade);
+        FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO, "Atualização efetuada com sucesso", ""));
+    }
+
+    public String limpCidade() {
+        cidade = new Cidade();
+        return "/restrict/cadastrarCidade.faces";
+    }
+    
+    public void deleteCidade() {
+        cidadeDAO().remove(cidade);
+    }
+
+    public Cidade getCidade() {
+        
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public List<Cidade> getCidades() {
+        cidades = cidadeDAO().getEntities();
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+    
+}
