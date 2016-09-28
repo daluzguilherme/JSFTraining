062ab7ccb5b72122e0344569c0be4a1b41e27952
Guilherme da Luz <guilherme.daluz@outlook.com>
2016-09-28 08:31:26 -0300

    Trocando SessionScoped do MB (Bug de gravação no banco) e removendo dependência do maven que estava no web.xml

--- JSF/src/main/java/br/com/controller/MbCidade.java
+++ JSF/src/main/java/br/com/controller/MbCidade.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -16,7 +16,7 @@ import javax.faces.context.FacesContext;
  * @author guilherme
  */
 @ManagedBean(name="mbCidade")
-@SessionScoped
+@RequestScoped
 public class MbCidade implements Serializable{
     
     private static final long serialVersionUID = 1L;
